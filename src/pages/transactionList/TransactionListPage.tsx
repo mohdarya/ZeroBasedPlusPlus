31af1613105b1e4244dcc8e49fd22dd402d61b4a
mohdarya <mohsen.a.daryanavard@gmail.com>
2023-12-14 08:32:34 +0400

    feat: added bottom bar

--- src/pages/transactionList/TransactionListPage.tsx
+++ src/pages/transactionList/TransactionListPage.tsx
@@ -1,7 +1,9 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 import {useNavigation} from '@react-navigation/core';
 import TopBar from './components/TopBar';
 import SpendingInfo from './components/SpendingInfo.tsx';
+import BottomBar from '../shared/components/BottomBar.tsx';
 
 interface TransactionListPageProps {}
 
@@ -16,6 +18,13 @@ function TransactionListPage(props: TransactionListPageProps) {
       backgroundColor: '#555B6E',
       justifyContent: 'space-between',
     },
+    bottomBarView: {
+      height: 60,
+      marginBottom: '5%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
   });
 
   return (
@@ -23,6 +32,9 @@ function TransactionListPage(props: TransactionListPageProps) {
       <TopBar />
 
       <SpendingInfo balanceText={'test'} balanceAmount={10} />
+      <View style={styles.bottomBarView}>
+        <BottomBar balanceText={'test'} balanceAmount={100} />
+      </View>
     </View>
   );
 }
