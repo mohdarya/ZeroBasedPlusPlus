170ed355b4e8da43b3fc2857ea8c6da2e053320d
mohdarya <mohsen.a.daryanavard@gmail.com>
2023-12-29 17:05:56 +0400

    refactor: changing search box

--- src/pages/categoryListPage/components/CategoryList.tsx
+++ src/pages/categoryListPage/components/CategoryList.tsx
@@ -59,18 +59,12 @@ function CategoryList(props: TransactionListProps) {
             width: '90%',
             height: '100%',
             borderRadius: 15,
-            backgroundColor: '#FAF9F9',
         }, searchBox: {
             display: 'flex',
-            flexDirection: 'row',
-            borderTopLeftRadius: 15,
-            borderTopRightRadius: 15,
             justifyContent: 'flex-start',
-            alignItems: 'center',
-            height: '10%',
             width: '100%',
-            marginBottom: 10,
-            marginLeft: 20,
+            alignItems: 'flex-start',
+
         }
 
 
@@ -79,8 +73,9 @@ function CategoryList(props: TransactionListProps) {
     return (
         <View style={styles.container}>
             <View style={styles.searchBox}>
-                <Icon name="search" size={25}/>
-                <TextInput placeholder={"Search"}/>
+                <Icon name="search"  style={{color: '#E9EEEA', backgroundColor: '#282828', padding: 10, borderRadius: 100}} onPress={() =>
+                    //@ts-ignore
+                    navigation.navigate('CategoryListPage')} size={25}/>
 
             </View>
             <ScrollView
